477991937ae0328a87ee208078a2fa48de582d79
jacklu333333 <jacklu333333@gmail.com>
2019-11-24 14:28:57 +0000

    case sensitive and mark annote , not slash word

--- PDF/test.py
+++ PDF/test.py
@@ -1,26 +1,38 @@
-# import packages
-import PyPDF2
-import PyPDF2Highlight
-import re
+import sys
+import fitz
+from PIL import Image, ImageColor
 
-# open the pdf file
-f = open("bias.pdf")
-object = PyPDF2.PdfFileReader(f)
 
-# get number of pages
-NumPages = object.getNumPages()
+def mark_word(page, text):
+    """Underline each word that contains 'text'.
+    """
+    found = 0
+    wlist = page.getTextWords()        # make the word list
+    for w in wlist:                    # scan through all words on page
+        if text in w[4]:               # w[4] is the word's string
+            found += 1                 # count
+            print("|"+w[4]+"|")
+            r = fitz.Rect(w[:4])       # make rect from word bbox
+            page.drawRect(r, color=ImageColor.getcolor(
+                "black", "L"), fill=ImageColor.getcolor("black", "L"), overlay=True)
+            # page.addUnderlineAnnot(r)  # underline
+    return found
 
-# define keyterms
-String = "Min"
 
-# extract text and do the search
-for i in range(0, NumPages):
-    PageObj = object.getPage(i)
-    print("this is page " + str(i))
-    Text = PageObj.extractText()
-    # print(Text)
-    ResSearch = re.search(String, Text)
-    print(ResSearch)
+fname = sys.argv[1]                    # filename
+text = sys.argv[2]                     # search string
+doc = fitz.open(fname)
 
-OUT = PyPDF2.PdfFileWriter()
-OUT.addHi
+print("underlining words containing '%s' in document '%s'" % (text, doc.name))
+
+new_doc = False                        # indicator if anything found at all
+
+for page in doc:                       # scan through the pages
+    found = mark_word(page, text)      # mark the page's words
+    if found:                          # if anything found ...
+        new_doc = True
+        print("found '%s' %i times on page %i" %
+              (text, found, page.number + 1))
+
+if new_doc:
+    doc.save("testoutput.pdf")
