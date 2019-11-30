70dca174c7932d71d6431defcd0e68d7df386ecf
Quentin Piot <qkpiot@gmail.com>
2019-11-30 13:51:49 +0900

    Start chrome extension

---
+++ chromeExtension/background.js
@@ -0,0 +1,27 @@
+
+    
+    
+    let downloadedFromScript = false;
+    let urlF;
+    chrome.downloads.onDeterminingFilename.addListener(function(downloadItem, suggest) {
+        let r = Math.floor(Math.random() * 1000).toString()
+       
+        if(downloadedFromScript){
+        name = r + downloadItem.id.toString() + downloadItem.filename;
+        suggest({filename: "bias/"+name, conflictAction: "overwrite" });
+        urlF = downloadItem.filename;
+        alert(urlF);
+        downloadedFromScript = false;
+    }
+      });
+
+
+      chrome.runtime.onMessage.addListener( function(request,sender,sendResponse)
+{
+    if( request.greeting  === "Download")
+    {
+        
+        downloadedFromScript = true;
+          
+    }
+      })
