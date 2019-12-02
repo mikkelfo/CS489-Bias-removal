00de48c9b05e02a929f980e0b6af0f759cc0ad6d
jacklu333333 <jacklu333333@gmail.com>
2019-12-02 00:30:27 +0000

    temp

--- PDF/Marker.py
+++ PDF/Marker.py
@@ -1,13 +1,12 @@
 import sys
 import fitz
-from PIL import Image, ImageColor
 
 annote_list = ["a", "b", "c", "d", "e", "f", "g"]
 
 
-def Marker(filename, target_name_list):
+def Marker(doc, target_name_list):
+    col = fitz.utils.getColor("white")
     changed = False
-    doc = fitz.open(filename)
     for page in doc:
         wordlist = page.getTextWords()
         for word in wordlist:
@@ -15,15 +14,64 @@ def Marker(filename, target_name_list):
                 if target in word:
                     changed = True
                     rectangle = fitz.Rect(word[:4])
-                    page.drawRect(rectangle, color=ImageColor.getcolor(
-                        "black", "L"), fill=ImageColor.getcolor("black", "L"), overlay=True)
+                    page.drawRect(rectangle, color=col, fill=col, overlay=True)
                 elif target in word[4]:
                     for annote in annote_list:
                         if target+annote in word[4]:
                             changed = True
                             rectangle = fitz.Rect(word[:4])
-                            page.drawRect(rectangle, color=ImageColor.getcolor(
-                                "black", "L"), fill=ImageColor.getcolor("black", "L"), overlay=True)
+                            page.drawRect(rectangle, color=col,
+                                          fill=col, overlay=True)
+                        elif annote+target in word[4]:
+                            changed = True
+                            rectangle = fitz.Rect(word[:4])
+                            page.drawRect(rectangle, color=col,
+                                          fill=col, overlay=True)
+    return [changed, doc]
+
+
+def Replace(doc, target_name_list):
+    black = fitz.utils.getColor("black")
+    white = fitz.utils.getColor("white")
+    changed = False
+    for page in doc:
+        wordlist = page.getTextWords()
+        for word in wordlist:
+            for target in target_name_list:
+                if target in word:
+                    changed = True
+                    rectangle = fitz.Rect(word[:4])
+                    page.drawRect(rectangle, color=white,
+                                  fill=white, overlay=True)
+                    result = -1
+                    size = 8
+                    print(rectangle.)
+                    while(result < 0):
+                        result = page.insertTextbox(
+                            rectangle, "GG", expandtabs=size, fill=black, overlay=True)
+                        size -= 1
+                        print("["+str(size)+"]")
+
+                        if size == 0:
+                            print("This is re "+str(result)+" "+target)
+                            break
+
+                elif target in word[4]:
+                    for annote in annote_list:
+                        if target+annote in word[4]:
+                            changed = True
+                            rectangle = fitz.Rect(word[:4])
+                            page.drawRect(rectangle, color=white,
+                                          fill=white, overlay=True)
+                            page.insertTextbox(
+                                rectangle, "AAA", fill=black, overlay=True)
+                        elif annote+target in word[4]:
+                            changed = True
+                            rectangle = fitz.Rect(word[:4])
+                            page.drawRect(rectangle, color=white,
+                                          fill=white, overlay=True)
+                            page.insertTextbox(
+                                rectangle, "buffer",  fontname="helv", expandtabs=8, fill=black, overlay=True)
     return [changed, doc]
 
 
@@ -38,7 +86,8 @@ def auto_processing(target_name_list):
     #####################################################
     #               Marking
     #####################################################
-    [new_doc, doc] = Marker("./bias.pdf", target_name_list)
+    doc = fitz.open("./bias.pdf")
+    [new_doc, doc] = Replace(doc, target_name_list)
 
     #####################################################
     #               Save File
@@ -50,5 +99,5 @@ def auto_processing(target_name_list):
 
 
 target_name_list = ["Andrew", "Tomkins",
-                    "Min", "Zhang", "William", "D.", "Heavlin"]
+                    "Min", "Zhang", "William", "D.", "Heavlin", "Peer", "Google"]
 auto_processing(target_name_list)
