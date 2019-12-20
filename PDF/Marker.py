3737288c5956fd567514d4d477a4c3c7a2508dc0
jacklu333333 <jacklu333333@gmail.com>
2019-12-20 10:54:57 +0900

    color add

--- PDF/Marker.py
+++ PDF/Marker.py
@@ -1,22 +1,20 @@
 import fitz
 
-def Replace(doc, target_name_list, type_name):
-    black = fitz.utils.getColor("black")
-    white = fitz.utils.getColor("white")
+def Replace(doc, target_name_list, type_name, thecolor):
     for page in doc:
         for target in target_name_list:
             touch_list = page.searchFor(target)
             for touch in touch_list:
                 if not abs(touch[0] - touch[2]) < 10:
-                    page.drawRect(touch, color=black,
-                                  fill=black, overlay=True)
-                    result = -1
-                    size = 8
-                    while(result < 0):
-                        result = page.insertTextbox(
-                            touch, type_name+" "+str(target_name_list.index(target)+1), fontsize=size, expandtabs=8, fill=white, overlay=True)
-                        size -= 1
-                        if size == 0:
-                            print("failed to replace")
-                            break
+                    page.drawRect(touch, color=thecolor,
+                                  fill=thecolor, overlay=False)
+                    #result = -1
+                    #size = 8
+                    #while(result < 0):
+                    #    result = page.insertTextbox(
+                    #        touch, type_name+" "+str(target_name_list.index(target)+1), fontsize=size, expandtabs=8, fill=white, overlay=True)
+                    #    size -= 1
+                    #    if size == 0:
+                    #        print("failed to replace")
+                    #        break
 
--- main.py
+++ main.py
@@ -14,16 +14,22 @@ def main(file):
     emails = preprocess.find_emails(text)
     multi_email = preprocess.find_multiple_emails(text)
 
+    red = fitz.utils.getColor("red")
+    yellow = fitz.utils.getColor("yellow")
+    blue = fitz.utils.getColor("blue")
+    green = fitz.utils.getColor("green")
+    cyan = fitz.utils.getColor("cyan")
+
     if 'PERSON' in ents:
-        Marker.Replace(doc, ents['PERSON'], "Person")
+        Marker.Replace(doc, ents['PERSON'], "Person",red)
     if 'ORG' in ents:
-        Marker.Replace(doc, ents['ORG'], "Org")
+        Marker.Replace(doc, ents['ORG'], "Org",yellow)
     if 'GPE' in ents:
-        Marker.Replace(doc, ents['GPE'], "Location")
+        Marker.Replace(doc, ents['GPE'], "Location",blue)
     if emails:
-        Marker.Replace(doc, emails, "email")
+        Marker.Replace(doc, emails, "email",green)
     if multi_email:
-        Marker.Replace(doc, multi_email, "multiple@ ")
+        Marker.Replace(doc, multi_email, "multiple@ ",cyan)
 
     doc.save("output.pdf")
     #doc.save(file[:-4] + "_edited" + file[-4:])
